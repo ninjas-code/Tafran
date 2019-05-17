5242520b61c6e0712ff1d982d784de3673175c68
Qusai <qusaiznemat6@gmail.com>
2019-05-18 00:27:29 +0300

    The Route is Working Now

--- src/App.jsx
+++ src/App.jsx
@@ -3,68 +3,22 @@ import ReactDOM from 'react-dom'
 import logo, { ReactComponent } from './logo.svg';
 import './App.css';
 import $ from 'jquery'; 
+import { BrowserRouter , Route} from 'react-router-dom';
+import Food from './components/food.jsx';
 
 
 class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      Price: "",
-      isHidden: true
-    }
-  }
-
-  toggleHidden () {
-    this.setState({
-      isHidden: !this.state.isHidden
-    })
-  }
-
-  handlePriceChange (e) {
-    console.log(e.target.value)
-    this.setState({
-     Price : e.target.value,
-    });
-
-  }
-
-  sendPriceToServer(e) {
-    console.log(this.state.Name )
-    
-    var obj = {
-      name: this.state.Price
-    }
-        var that=this;
-        $.ajax({
-          method: "POST",
-          data: {
-      name: this.state.Price
-    },
-          url: 'http://127.0.0.1:3000/price', 
-          dataType: "application/json",
-    
-          success: (data) => {
-            that.setState({
-              items: data
-            })
-          },
-          error: (err) => {
-            console.log('err', err);
-          }
-        });
-      }    
-    render(){
+   /* <Route path="/test" component={test} />
+    <Route path="/test1" component={Food} />*/ 
+render(){
   return (
+    <BrowserRouter>
+    
     <div className="App">
-      <header className="App-header">
-        <form method="POST">
-        <h1 className="title">Put the Price</h1>
-        <input className="Input" placeholder="in how mutch you want to eat" name="price"/>
-        <button className="button">EAT</button>
-        </form>
-      </header>
-    </div>
+      <Route path="/" component={Food} />
+      </div>
+     
+    </BrowserRouter>
   );
   }
 }
