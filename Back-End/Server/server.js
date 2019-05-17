f4f79b1833b3c29ccc2847d4d2cfd52b5d4f69f1
Qusai <qusaiznemat6@gmail.com>
2019-05-17 14:39:40 +0300

    DATABASE MORE BUG Fixing

--- Back-End/Server/server.js
+++ Back-End/Server/server.js
@@ -8,23 +8,83 @@ const connection = mysql.createConnection({
     host : 'localhost',
     user:'root',
     password:"1111",
-    //database: 'appData'
+   database: 'restaurants'
 });
 
 // Create DB
-app.get("/CR",(req,res)=>{
-  let sql = 'CREATE DATABASE appData'
+app.get("/CRDATA",(req,res)=>{
+  let sql = 'CREATE DATABASE if not exists restaurants'
   connection.query(sql,(err,result)=>{
     if(err){
       throw err
     };
     console.log(result);
-    res.send("It's Working")
+    res.send("The Database was created successfully")
     console.log("Data Base Created")
     connection.end();
 
+  });
+});
+//create table inside DB
+app.get("/CRTable",(req,res)=>{
+
+  let CreateTable = `CREATE TABLE if not exists restaurants(
+   id int primary key AUTO_INCREMENT,
+   Name VARCHAR(255),
+   address VARCHAR(255),
+   Food VARCHAR(500),
+   Phonenumber int
+   )`
+    connection.query(CreateTable,(err,result)=>{
+      if(err) throw err;
+      console.log(result);
+      console.log("Table Was Created On Successfully")
+      res.send("Table Was Created")
+    })
+  });
+   // Create User Inside The Databasce
+   app.get("/CN",(req,res)=>{
+  let newRestaurant = {Name:'Mcdonales',address:"Amman",Food:"hamburger",Phonenumber:'07757231'}
+  let sql = 'INSERT into restaurants ?';
+  let query = connection.query(sql,newRestaurant,(err,result)=>{
+    if(err) throw err;
+    console.log(result);
+    res.send("User Was Added")
+
   })
 })
+
+// Search Into the database and appear all the data
+app.get("/getUsers",(req,res)=>{
+  let serchItem = 'SELECT * FROM restaurants';
+  let query = connection.query(serchItem,(err,result)=>{
+    if(err) throw err;
+    console.log(result);
+    res.send("User Was Added")
+
+  });
+});
+
+// Search Into the database and appear all the data
+app.get("/getoneUser",(req,res)=>{
+  let serchItem = 'SELECT * FROM restaurants WHERE Name'
+  let query = connection.query(serchItem,(err,result)=>{
+    if(err) throw err;
+    console.log(result);
+    res.send("User Was Added")
+
+  });
+});
+
+
+
+
+//     connection.query(CreateTable,(err,result)=>{
+//     if(err) throw err
+//     console.log(result);
+//     res.send('The Restarurants Table Created');
+//   })
+// })
 
 // The connection made
 connection.connect((err)=>{
