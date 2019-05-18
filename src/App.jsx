7bc33ccb559b18c157bb7e45c5f1379f10f4c5af
RaedShihab <raedshihab92@gmail.com>
2019-05-18 22:22:37 +0300

    send data to the server1

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,15 @@ import Food from './components/food.jsx';
 import Err from './components/404Page.jsx';
 import Navgation from './components/Nav.jsx';
 import Login from './components/login.jsx';
+import MealsList from './components/mealsList';
 
 
 class App extends React.Component {
    /* <Route path="/test" component={test} />
     <Route path="/test1" component={Food} />*/ 
+
+
+    
 render(){
   return (
     <BrowserRouter>
@@ -20,7 +24,7 @@ render(){
     <div className="App">
       
       <Navgation></Navgation>
-
+    <MealsList meals ={[{name:'meal1',price:2},{name:'meal2',price:4},{name:'meal3',price:3},{name:'meal4',price:5}]}/>
       <Switch>
       <Route path="/" component={Food} exact/>
       <Route path="/login" component={Login} exact/>
