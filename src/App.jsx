ad9fc4aa3ae241c6d55fb1a91d982cfd30d3e4d9
wael Ashi <waelashi1@gmail.com>
2019-05-20 01:19:33 +0300

    DeletNav2

--- src/App.jsx
+++ src/App.jsx
@@ -28,14 +28,14 @@ render(){
     {/* <MealsList meals ={[{name:'meal1',price:2},{name:'meal2',price:4},{name:'meal3',price:3},{name:'meal4',price:5}]}/> */}
       
       <Route  path="/" component={Food} exact />
-      <Route  path="/login" component={Login} exact />
+      {/* <Route  path="/login" component={Login} exact />
       <Route  path="/meals" render={(props) =>
-       <MealsList meals ={[{name:'meal1',price:2, resturant: 'Bab-Alyamen'},{name:'meal2',price:4, resturant: 'Bab-Alyamen'},{name:'meal3',price:3, resturant: 'Jabri'},{name:'meal4',price:5, resturant: 'AAlya'}]}/> } />
+       <MealsList meals ={[{name:'meal1',price:2, resturant: 'Bab-Alyamen'},{name:'meal2',price:4, resturant: 'Bab-Alyamen'},{name:'meal3',price:3, resturant: 'Jabri'},{name:'meal4',price:5, resturant: 'AAlya'}]}/> } /> */}
 
-        <Route  path="/resturant" render={(props) =>
-       <Resturant meals ={[{name:'meal1',price:2, resturant: 'Hbiba'},{name:'meal2',price:4, resturant: 'Bab-Alyamen'},{name:'meal3',price:3, resturant: 'Jabri'},{name:'meal4',price:5, resturant: 'AAlya'}]}/> } />      
+        {/* <Route  path="/resturant" render={(props) =>
+       <Resturant meals ={[{name:'meal1',price:2, resturant: 'Hbiba'},{name:'meal2',price:4, resturant: 'Bab-Alyamen'},{name:'meal3',price:3, resturant: 'Jabri'},{name:'meal4',price:5, resturant: 'AAlya'}]}/> } />       */}
 
-      <Route component={Err} exact />
+      {/* <Route component={Err} exact /> */}
       
       </div>
      
