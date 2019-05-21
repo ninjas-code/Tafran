d75888893bcccc63bcc10a25d60a3e49d683f31e
Qusai <qusaiznemat6@gmail.com>
2019-05-21 13:05:52 +0300

    Adding instruction

--- Back-End/Server/server.js
+++ Back-End/Server/server.js
@@ -9,22 +9,21 @@ const pino = require('express-pino-logger');
 const Core = require('cors');
 const testAPIRouter = require("./testAPI.js")
 // const JSON = require('circular-json');
-app.use(Core());
-app.use("/testAPP",testAPIRouter)
-// Add headers
+app.use(Core()); // to solve the Proxy Problem
+app.use("/testAPP",testAPIRouter) // To Conact the Router to the server
+/*This the Solve the access to the server problem - qusai*/ 
 app.use("http://localhost:3000/",function (req, res, next) {
 
-  // Website you wish to allow to connect
+
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
-  // Request methods you wish to allow
+
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-  // Request headers you wish to allow
+
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
+
   res.setHeader('Access-Control-Allow-Credentials', true);
 
   // Pass to next layer of middleware
