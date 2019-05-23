3ce4e5c970a4fab0f286f4bab22f99e62b239697
wael Ashi <waelashi1@gmail.com>
2019-05-23 19:30:05 +0300

    RemoveResturantFile

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,6 @@
 import React  from 'react';
 import {NavLink} from 'react-router-dom';
-// import {MealList} from 'mealsList.jsx'
-// import React  from 'react';
-// import {NavLink} from 'react-router-dom';
+
 
 const Navgation =() =>{
 
